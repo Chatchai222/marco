230e91211fd011b546de7e3071ed9d0e3fd26aff
Chatchai Paisalpanich <frinttropystudy222@gmail.com>
2023-09-29 10:46:38 +0700

    Added getLocationById for location

--- src/main/java/com/example/marco/location/LocationController.java
+++ src/main/java/com/example/marco/location/LocationController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,12 +15,17 @@ public class LocationController {
     private final LocationService locationService;
     
     @Autowired
-    public LocationController(LocationService inLocationService){
-        this.locationService = inLocationService;
+    public LocationController(LocationService in_location_service){
+        this.locationService = in_location_service;
     }
 
     @GetMapping
     public List<Location> getAllLocation() {
         return locationService.getAllLocation();
+    }
+
+    @GetMapping("/{location_id}")
+    public Location getLocationById(@PathVariable Long location_id){
+        return locationService.getLocationById(location_id);
     }
 }
--- src/main/java/com/example/marco/location/LocationService.java
+++ src/main/java/com/example/marco/location/LocationService.java
@@ -1,6 +1,7 @@
 package com.example.marco.location;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,15 +9,23 @@ import org.springframework.stereotype.Service;
 @Service
 public class LocationService {
 
-    private final LocationRepository locationRepository;
+    private final LocationRepository location_repository;
     
     @Autowired
-    public LocationService(LocationRepository inLocationRepository){
-        this.locationRepository = inLocationRepository;
+    public LocationService(LocationRepository in_location_repository){
+        this.location_repository = in_location_repository;
     }
 
     public List<Location> getAllLocation(){
-        return this.locationRepository.findAll();
+        return this.location_repository.findAll();
+    }
+
+    public Location getLocationById(Long location_id){
+        Optional<Location> opt_location = this.location_repository.findById(location_id);
+        if (!opt_location.isPresent()){
+            throw new IllegalStateException("Location with id: " + location_id + " does not exist");
+        }
+        return opt_location.get();
     }
 
 }
