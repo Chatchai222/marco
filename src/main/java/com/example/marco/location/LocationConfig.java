36b2bcd22268b9d490c08b324a58590be9ea7e2b
Chatchai Paisalpanich <frinttropystudy222@gmail.com>
2023-09-29 10:16:04 +0700

    Changed floor to 8th

--- src/main/java/com/example/marco/location/LocationConfig.java
+++ src/main/java/com/example/marco/location/LocationConfig.java
@@ -12,8 +12,8 @@ public class LocationConfig {
     @Bean
     public CommandLineRunner commandLineRunner(LocationRepository repository) {
         return args -> {
-            Location ecc_804 = new Location("ECC804", "Eigth Floor", 23.4, 24.2);
-            Location prof_pipat_office = new Location("Prof Pipat Office", "Eigth floor", 32.4, 25.5);
+            Location ecc_804 = new Location("ECC804", "8th floor", 23.4, 24.2);
+            Location prof_pipat_office = new Location("Prof Pipat Office", "8th floor", 32.4, 25.5);
             
             repository.saveAll(List.of(ecc_804, prof_pipat_office));
         };
