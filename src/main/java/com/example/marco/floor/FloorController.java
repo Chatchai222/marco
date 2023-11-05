fb4486effa57d09db3a0eb3cfa8d2223011f960d
Chatchai Paisalpanich <frinttropystudy222@gmail.com>
2023-11-05 15:22:28 +0700

    Added API for getAllFloor

--- src/main/java/com/example/marco/floor/FloorController.java
+++ src/main/java/com/example/marco/floor/FloorController.java
@@ -1,5 +1,8 @@
 package com.example.marco.floor;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,12 +11,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "api/v1/floor")
 public class FloorController {
     
+    private final FloorService floorService;
+
+    @Autowired
+    public FloorController(FloorService inFloorService){
+        this.floorService = inFloorService;
+    }
+
     @GetMapping("hello")
     public String getHello(){
         return "Hello, you tried getting in floor apis";
     }
 
-    
+    @GetMapping("all")
+    public List<Floor> getAllFloor(){
+        return floorService.getAllFloor();
+    }
 
+    
 
 }
