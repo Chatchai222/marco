dd0ef9d666730e972bf62f862ec57dd3ef0f237b
Chatchai Paisalpanich <frinttropystudy222@gmail.com>
2023-12-18 14:55:09 +0700

    yoinked from https://frontbackend.com/spring-boot/spring-boot-upload-file-to-postgresql-database

---
+++ src/main/java/com/example/marco/file/FileEntity.java
@@ -0,0 +1,73 @@
+package com.example.marco.file;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
+public class FileEntity {
+    
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String id;
+
+    private String name;
+    private String contentType;
+    private Long size;
+
+    @Lob
+    private byte[] data;
+
+    public FileEntity(){
+
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContentType() {
+        return contentType;
+    }
+
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
+
+    public Long getSize() {
+        return size;
+    }
+
+    public void setSize(Long size) {
+        this.size = size;
+    }
+
+    public byte[] getData() {
+        return data;
+    }
+
+    public void setData(byte[] data) {
+        this.data = data;
+    }
+
+
+    
+}
---
+++ src/main/java/com/example/marco/file/FileRepository.java
@@ -0,0 +1,9 @@
+package com.example.marco.file;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+@Repository
+public interface FileRepository extends JpaRepository<FileEntity, String>{
+    
+}
