08d201b8fcf0588c66790219b910403d8b0ec7b7
Chatchai Paisalpanich <frinttropystudy222@gmail.com>
2024-01-04 11:33:51 +0700

    Change controller mapping from /beacon to /beacons

--- src/main/java/com/example/marco/beacon/BeaconController.java
+++ src/main/java/com/example/marco/beacon/BeaconController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/v1/beacon")
+@RequestMapping("api/v1/beacons")
 public class BeaconController {
     
     final private BeaconService beaconService;
