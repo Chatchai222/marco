2dfc1b84444fbea6e6e854637f724b026b097e5c
Chatchai Paisalpanich <frinttropystudy222@gmail.com>
2024-02-02 16:29:31 +0700

    Added azimuth of ecc7th and ecc8th floor

--- src/main/java/com/example/marco/cmdconfig/HardcodeConfig.java
+++ src/main/java/com/example/marco/cmdconfig/HardcodeConfig.java
@@ -43,8 +43,8 @@ public class HardcodeConfig {
         return args -> {
             BuildingEntity eccBuilding = buildingService.addBuildingEntity(new BuildingEntity("ECC building"));
             
-            FloorEntity ecc7thFloor = floorService.addFloorEntity(new FloorEntity("ECC 7th floor", 39.6, 73.6, 999.99));
-            FloorEntity ecc8thFloor = floorService.addFloorEntity(new FloorEntity("ECC 8th floor", 37.4, 73.4, 999.99));
+            FloorEntity ecc7thFloor = floorService.addFloorEntity(new FloorEntity("ECC 7th floor", 39.6, 73.6, 270.00));
+            FloorEntity ecc8thFloor = floorService.addFloorEntity(new FloorEntity("ECC 8th floor", 37.4, 73.4, 270.00));
 
             List<LocationEntity> ecc7thLocationList = List.of(
                 locationService.addLocationEntity(new LocationEntity("ECC7-Back-Left-Toilet-Door", 15.20, 12.40)),
